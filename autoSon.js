5a5cc5b6b07bccdf4620935be3039b59d2d77352
cuongdc <cuongdc@mindx.vn>
2024-07-05 23:40:43 +0700

    implement auto click

---
+++ autoSon.js
@@ -0,0 +1,44 @@
+const axios = require("axios");
+
+let availableTaps = 0;
+let balanceCoins = 0;
+const callApi = async () => {
+  try {
+    const { data } = await axios({
+      method: "post",
+      url: "https://api.hamsterkombat.io/clicker/tap",
+      headers: {
+        "Accept-Language": "en,vi-VN;q=0.9,vi;q=0.8,en-US;q=0.7",
+        Connection: "keep-alive",
+        Origin: "https://hamsterkombat.io",
+        Referer: "https://hamsterkombat.io/",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-site",
+        "User-Agent":
+          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+        accept: "application/json",
+        authorization:
+          "Bearer 1720197421439rcvWrRloj5a3dfmcqWA0ZAvnGkVzEHrt5rt48tEeWjfJuvezNFC4V9dQ5SiffWMZ5651290029",
+        "content-type": "application/json",
+        "sec-ch-ua":
+          '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Linux"',
+      },
+      data: {
+        count: Math.floor(Math.random() * 10) + 1,
+        availableTaps,
+        timestamp: Math.floor(Date.now() / 1000),
+      },
+    });
+
+    availableTaps = data?.clickerUser?.availableTaps || 0;
+    balanceCoins = data?.clickerUser?.balanceCoins || 0;
+    console.log({ balanceCoins });
+  } catch (error) {
+    console.error(error?.request?.data);
+  }
+};
+
+setInterval(callApi, 2000);
